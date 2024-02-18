d2105d247cc86c159d06f420fdcee8c178af1820
Aletia88 <abababmgmgmg@gmail.com>
2024-02-19 01:08:24 +0300

    footer update

--- src/components/FooterR.vue
+++ src/components/FooterR.vue
@@ -4,7 +4,7 @@
         <div className="flex flex-wrap justify-between">
           <div className="w-full md:w-1/3 mb-4 md:mb-0 border-r-2">
             <h4 className="text-xl font-bold mb-4 uppercase">Recipe Realm</h4>
-            <p className="text-sm">"Discover Ethiopia A Tapestry of Tradition, Nature, and Endless Wonder."</p>
+            <p className="text-sm">""</p>
         </div>
         <div>
             <h1>Contact Us</h1>
