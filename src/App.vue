3e4e33f84a95f8c7022bfc3b642958db6ac754d1
Aletia88 <abababmgmgmg@gmail.com>
2024-02-20 23:38:34 +0300

    routing

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,16 @@ import BrowseRecipe from "./components/pages/BrowseRecipe.vue";
 import CreateRecipe from "./components/pages/CreateRecipe.vue";
 import About from "./components/pages/About.vue";
 import FooterR from "./components/FooterR.vue";
+import LoginPage from "./components/views/LoginPage.vue";
+import RegisterationPage from "./components/views/RegisterationPage.vue";
 
 const routes = {
   "/": Home,
   "/browse": BrowseRecipe,
   "/create": CreateRecipe,
   "/about": About,
+  "/login": LoginPage,
+  "/register": RegisterationPage
 };
 
 const currentPath = ref(window.location.hash);
@@ -23,16 +27,26 @@ window.addEventListener("hashchange", () => {
 const currentView = computed(() => {
   return routes[currentPath.value.slice(1) || "/"] || NotFound;
 });
+
+const showHeaderFooter = computed(() => {
+  return currentPath.value !== "#/login" && currentPath.value !==  "#/register";
+})
 </script>
 
+
+
 <template>
-  <header>
+<div>
+  <header v-if="showHeaderFooter">
     <HeaderR />
   </header>
   <main>
+    
     <component :is="currentView" />
   </main>
-  <footer>
+  <footer v-if="showHeaderFooter">
     <FooterR />
   </footer>
+</div>
+  
 </template>
