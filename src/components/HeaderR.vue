8a74d0c18c84cecbfc6bb024d05e1da87bfefcd1
Aletia88 <abababmgmgmg@gmail.com>
2024-02-21 00:26:03 +0300

    searchbar

--- src/components/HeaderR.vue
+++ src/components/HeaderR.vue
@@ -36,13 +36,13 @@
         <div class="flex justify-between mx-5 bg-mainc items-center ">
             <a href="#/" class="text-white font-bold relative duration-500 tracking-wider">RECIPE REALM </a>
             <!-- Use PascalCase for component name -->
-            <SearchBar />
+            
             <ul class="flex gap-7  h-fit text-white p-2 px-14 rounded-lg items-center tracking-wide">
-                <li><a href="#/">Home</a></li>
+                <li><a href="#/" >Home</a></li>
                 <li><a href="#/browse">Browse Recipe</a></li>
                 <li><a href="#/create">Create Recipe</a></li>
                 <li><a href="#/about">About</a></li>
-                <li class="bg-[#F4CE14] px-3 py-1 text-black rounded "><a href="#/login">Sign In</a></li>
+                <button class="bg-[#F4CE14] px-3 py-1 text-black rounded "><a href="#/login">Sign In</a></button>
                 <!-- <router-link to="/login">Sign In</router-link> -->
                
             </ul>
@@ -51,15 +51,10 @@
 </template>
 
 <script>
-// Import the SearchBar component
-import SearchBar from './SearchBar.vue';
+
 import RegisterationPage from './views/RegisterationPage.vue';
 
-export default {
-    components: {
-        SearchBar // Register the SearchBar component
-    }
-}
+
 </script>
 
 <style>
@@ -67,6 +62,10 @@ export default {
 nav.stickyy {
     padding:4px 10px;
     background-color: #fff;
+}
+nav.stickyy li:hover{
+    border-bottom: #000 1px solid;
+    transition-duration: 300ms;
 }
 nav.stickyy p {
     letter-spacing: 0.1em;
