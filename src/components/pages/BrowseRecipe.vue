5ba6cf23e98bb0485eb1a871b8d0d98dd59bbcf3
Aletia88 <abababmgmgmg@gmail.com>
2024-02-21 00:26:39 +0300

    catagory added

--- src/components/pages/BrowseRecipe.vue
+++ src/components/pages/BrowseRecipe.vue
@@ -1,61 +1,61 @@
 <template>
-  <div class=" h-[200vh] mt-24">
-    <nav class="z-10  pl-12 top-14 bg-white w-full sticky">
-      <div class="flex  px- w-fit">
+  <div class=" h-[200vh] pt-20 ">
+    <nav class=" border-b-[#F4CE14] border-solid border-b-1 z-10  pl-12 top-14 bg-white w-full sticky flex justify-around py-2 ">
+      <div class="flex  w-fit">
         <div class=" relative py-1 group">
-          <a href="" class=" px- py-1  ">Food Catagory</a>
+          <a href="" class=" px- py-1 tracking-wide visited:bg-black">Food Catagory</a>
           <div class="hidden absolute left-2 group-hover:block">
+            <ul class="bg-white text-start border-t-2 border-t-[#11111f] ">
+              <li class="border-b hover:border-b-[#f4ce14]  py-1"><a class=" px-10 w-full " href="">Breakfast</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Lunch</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Appetizers</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Salads</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Soups</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Desserts</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Breads</a></li>
+           
+            </ul>
+          </div>
+        </div>
+        <div class=" px-2 py-1 relative group">
+          <a href="" class=" px-3 py-1 tracking-wide">Ingredient</a>
+          <div class="hidden absolute group-hover:block">
             <ul class="bg-white text-center ">
-              <li class="border px-10 py-1 "><a href="">Vegiterian</a></li>
-              <li class="border px-10 py-1">Vegiterian</li>
-              <li class="border px-10 py-1">Vegiterian</li>
-              
-             
-              
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+           
             </ul>
           </div>
         </div>
         <div class=" px-2 py-1 relative group">
-          <a href="" class=" px-3 py-1">Ingredient</a>
+          <a href="" class=" px-3 py-1 tracking-wide">Creator</a>
           <div class="hidden absolute group-hover:block">
-            <ul class="bg-white text-center">
-              <li class="border py-1">Vegiterian</li>
-              <li class="border py-1">Vegiterian</li>
-              <li class="border py-1">Vegiterian</li>
-              <li>Vegiterian</li>
-             
-              
+            <ul class="bg-white text-center ">
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+           
             </ul>
           </div>
         </div>
         <div class=" px-2 py-1 relative group">
-          <a href="" class=" px-3 py-1">Creator</a>
+          <a href="" class=" px-3 py-1 tracking-wide">Preparation Time</a>
           <div class="hidden absolute group-hover:block">
-            <ul class="bg-white text-center">
-              <li class="border px-3 py-1">Vegiterian</li>
-              <li class="border px-3 py-1">Vegiterian</li>
-              <li class="border px-3 py-1">Vegiterian</li>
-              <li>Vegiterian</li>
-             
-              
-            </ul>
-          </div>
-        </div>
-        <div class=" px-2 py-1 relative group">
-          <a href="" class=" px-3 py-1">Preparation Time</a>
-          <div class="hidden absolute group-hover:block">
-            <ul class="bg-white text-center">
-              <li class="border py-1">Vegiterian</li>
-              <li class="border py-1">Vegiterian</li>
-              <li class="border py-1">Vegiterian</li>
-              <li>Vegiterian</li>
-             
-              
+            <ul class="bg-white text-center ">
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+              <li class="border-b  py-1"><a class=" px-10 w-full " href="">Vegiterian</a></li>
+           
             </ul>
           </div>
         </div>
         
       </div>
+      <SearchBar />
     </nav>
     <div class="flex w-full justify-center flex-wrap top-14 relative">
       <recipe-item 
@@ -74,10 +74,12 @@
   
   <script>
   import RecipeItem from '../RecipeItem.vue';
-  
+  import SearchBar from '../SearchBar.vue';
+
   export default {
     components: {
-      'recipe-item': RecipeItem
+      'recipe-item': RecipeItem,
+      SearchBar
     },
     data() {
       return {
